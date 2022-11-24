1ba42867ae6ff0ecfba9a7af2e28c3cdbde1df7c
Alexis-With-logic <119010724+Alexis-With-logic@users.noreply.github.com>
2022-11-24 14:47:54 -0600

    Update dorrat-inicio.js

--- dorrat-inicio.js
+++ dorrat-inicio.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('DORRAT-BOT-MD', {
+say('MULTIVERSE-BOT-MD', {
 font: 'tiny',
 align: 'center',
 colors: ['#ff8000']})
-say(`CREADO POR DIEGO-OFC`, {
+say(`CREADO POR ALEXIS_WITH-LOGIC`, {
 font: 'console',
 align: 'center',
 colors: ['red']})
